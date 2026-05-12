0d7d9b59d118c9550a104795245507880b60318b
kagika <r.m.kagika@gmail.com>
2026-05-12 11:31:27 +0300

    landing page

--- src/pages /Home.jsx
+++ src/pages /Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import '../css/Home.css'
+import Navbar from '../components/Navbar'
+
 
 function Home() {
   return (
     <main >
       <div className="overlay">
         <div className="landing-page">
-          
+          <Navbar/>
         </div>
       </div>
       
