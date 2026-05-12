d2dc91847884fd2bd35b5ed37f83b82ab248f397
kagika <r.m.kagika@gmail.com>
2026-05-12 10:59:13 +0300

    Updates for the landing page, pages folder and css folder added

---
+++ src/pages /Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/Home.css'
+
+function Home() {
+  return (
+    <main >
+      <div className="overlay">
+        <div className="landing-page">
+          
+        </div>
+      </div>
+      
+    </main>
+    
+  )
+}
+
+export default Home
